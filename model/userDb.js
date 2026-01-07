6ea520e7d163c7a525e2531edc724c7a11d11535
T0mzyy <jaideepkollys@gmail.com>
2026-01-07 13:40:29 +0530

    hh

--- model/userDb.js
+++ model/userDb.js
@@ -51,6 +51,10 @@ const userSchema = new mongoose.Schema(
       type: Number,
       default: 0,
     },
+    attentedCount: {
+      type: Number,
+      default: 0,
+    },
   },
   { timestamps: true }
 );
