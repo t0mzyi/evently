f5cb207ec34a93e802fc16f81f2b51cb1da169e5
T0mzyy <jaideepkollys@gmail.com>
2026-03-25 00:18:15 +0530

    url changed

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,7 @@ passport.use(
     {
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "https://jaideepdev.xyz/auth/google/callback",
+      callbackURL: "https://evently.jaideepdev.xyz/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
